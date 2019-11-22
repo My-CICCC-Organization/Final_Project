0a85b4e86757dda86e0b5336ac9c9ae67b69fceb
Anzu <anzunakayama@gmail.com>
2019-11-22 15:06:04 -0800

    edit person

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Person {
+
+    String UUID;
+    String name;
+    Date birthday;
+    double weightLb;
+    double weightKg;
+    int takenQuantity;
+    int status;
+
+    public int calAge(){
+        //(今日の日付-誕生日)/10000の小数点以下切捨て。
+        Date date=new Date();
+        DateFormat dateFormat=new SimpleDateFormat("yyyyMMdd");
+
+        String formattedDate=dateFormat.format(date);
+
+        System.out.println(formattedDate);
+        return 0;
+    }
+
+
 }
