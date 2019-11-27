b935c695be9c9938356deaa5cca4a2cc41ed3939
taekimura <55253319+taekimura@users.noreply.github.com>
2019-11-26 16:06:26 -0800

    modified-Record

--- src/main/java/controller/Record.java
+++ src/main/java/controller/Record.java
@@ -33,7 +33,6 @@ public class Record {
     }
 
     public static Map readPersonalFileAsMap() {
-
         String infoString = read(Constants.PERSONAL_INFO);;
         List<String> infoList = Arrays.asList(infoString.split(","));
         Map<String, String> infoMap = new HashMap<>();
@@ -43,11 +42,9 @@ public class Record {
         infoMap.put(Constants.WEIGHT, infoList.get(3));
         infoMap.put(Constants.STATUS, infoList.get(4));
         return infoMap;
-
     }
 
     public static Map readRecodeFileAsMap() throws IOException {
-
         String infoString = read(Constants.RECORD);
         List<String> infoList = Arrays.asList(infoString.split(","));
         Map<String, String> infoMap = new HashMap<>();
@@ -59,7 +56,5 @@ public class Record {
         infoMap.put(Constants.TAKEN_QUANTITY, infoList.get(5));
         infoMap.put(Constants.TIME, infoList.get(6));
         return infoMap;
-
         }
     }
-
