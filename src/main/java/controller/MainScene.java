a636238eff18932a086e2d7e328ec735582bcdc1
Diego <espinosa9mx@gmail.com>
2019-11-27 16:51:03 -0800

    update Setup

--- src/main/java/controller/MainScene.java
+++ src/main/java/controller/MainScene.java
@@ -1,39 +1,49 @@
 package controller;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
-import model.Person;
-import util.Constants;
-import util.StageManager;
+        import javafx.animation.Animation;
+        import javafx.animation.KeyFrame;
+        import javafx.animation.Timeline;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Label;
+        import javafx.scene.text.Text;
+        import javafx.util.Duration;
+        import model.Person;
+        import util.Constants;
+        import util.StageManager;
 
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class MainScene implements Initializable {
+public class MainScene {
+    @FXML
+    public Label amountThisTime;
 
     @FXML
-    private Label nextDrinkTimer;
-    @FXML
-    private Label amountThisTime;
-    @FXML
-    private Label lb_time;
-    @FXML
-    private Text lb_good;
+    public Label nextDrinkTimer;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    public Label lb_time;
+    @FXML
+    public Text lb_good;
+
+    private int hour;
+
+
+
+    private Setup a = new Setup();
+    public Person p = new Person();
+
+    public void initialize() throws IOException {
+
+
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
-            int hour = LocalDateTime.now().getHour();
-            if (hour > 7 && hour < 12){
+            hour = LocalDateTime.now().getHour();
+
+
+            if(hour > 7 && hour < 12){
                 lb_good.setText("Good Morning, " + Person.getName());
             }
             if(hour >= 12 && hour < 18){
@@ -42,9 +52,11 @@ public class MainScene implements Initializable {
             if(hour >= 18 && hour < 24){
                 lb_good.setText("Good Night, "+ Person.getName());
             }
+
         }),
                 new KeyFrame(Duration.seconds(1))
         );
+
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
 
@@ -68,3 +80,5 @@ public class MainScene implements Initializable {
         StageManager.minimizeStage(Constants.PRIMARY_STAGE);
     }
 }
+
+
--- src/main/java/model/Bottle.java
+++ src/main/java/model/Bottle.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Bottle {
+
+
+        public static void set_Size(String info){
+            try (FileWriter fw = new FileWriter("bottle", true);
+                 BufferedWriter bw = new BufferedWriter(fw);
+                 PrintWriter out = new PrintWriter(bw)){
+                 out.println(info);
+                System.out.println("*Data Saved in memo.txt");
+                bw.close();
+                out.close();
+            } catch (IOException e) {
+                System.out.println("Some Problems bro!");
+            }
+        }
+
 }
